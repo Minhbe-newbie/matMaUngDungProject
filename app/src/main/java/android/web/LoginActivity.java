ce5e81654bc80649d406490098b17ea6f0ed6036
MAC <65aminh2001@gmail.com>
2022-12-06 16:23:37 +0700

    fix issue form manifests

--- app/src/main/java/android/web/LoginActivity.java
+++ app/src/main/java/android/web/LoginActivity.java
@@ -6,16 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
     private String loginEmail , loginPassword ;
 
     EditText edtLoginEmail,edtLoginPassword;
     Button btnLogin;
-    View.OnClickListener ClickEvent = null;
 
 
     @Override
@@ -23,16 +21,16 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        edtLoginEmail = (EditText) findViewById(R.id.edtLoginEmail);
-        edtLoginPassword = (EditText) findViewById(R.id.edtLoginPassword);
+        edtLoginEmail =  findViewById(R.id.edtLoginEmail);
+        edtLoginPassword = findViewById(R.id.edtLoginPassword);
 
-        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin =  findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loginEmail = (edtLoginEmail.getText().toString());
                 loginPassword = (edtLoginPassword.getText().toString());
-                System.out.println(loginEmail + " and " + loginPassword + " is:");
+                Toast.makeText(LoginActivity.this, loginEmail +loginPassword, Toast.LENGTH_SHORT).show();
             }
         });
 
