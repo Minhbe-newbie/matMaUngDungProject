a40afae66205229b6208fed4e9156ddb73c5f80c
MAC <65aminh2001@gmail.com>
2022-12-16 13:36:38 +0700

    Add thằng main page

---
+++ app/src/main/java/android/web/MainHomeActivity.java
@@ -0,0 +1,13 @@
+package android.web;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_home);
+    }
+}
