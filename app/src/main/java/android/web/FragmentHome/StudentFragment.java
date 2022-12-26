7607ad7a8ebb9061db6a7943fe9ce8d070091c03
WhyLazyDoggo <cv301101@gmail.com>
2022-12-26 16:30:03 +0700

    StudentFragment.java: Đã thêm bắt sự kiện. Chưa có hiệu ứng kiểu giữ nút như nào. Chưa có bấm vào nút này sẽ mở giao diện kia mà chỉ là bắt sự kiện "Đã nhấn" thông thường fragment_student.xml: Đã thêm trường id cho từng Imagine View.
    Yêu cầu: Designer sửa lại cho các trường trong giao diện dãn cách ra nhiều chút, nhìn vô cùng díu dít nhau.

--- app/src/main/java/android/web/FragmentHome/StudentFragment.java
+++ app/src/main/java/android/web/FragmentHome/StudentFragment.java
@@ -7,9 +7,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.web.Login.LoginActivity;
 import android.web.R;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.sql.Connection;
 
 public class StudentFragment extends Fragment {
+
+    private View view;
+    private ImageView ivTTSV,ivCTH,ivDKT,ivLCN,ivTCD;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,7 +31,80 @@ public class StudentFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_student, container, false);
+        view = inflater.inflate(R.layout.fragment_student, container, false);
+
+        initUI();
+
         return view;
+    }
+
+    private void initUI() {
+        ivTTSV = view.findViewById(R.id.ivTTSV);
+        ivCTH = view.findViewById(R.id.ivCTH);
+        ivDKT = view.findViewById(R.id.ivDKT);
+        ivLCN = view.findViewById(R.id.ivLCN);
+        ivTCD = view.findViewById(R.id.ivTCD);
+
+        ivTCD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Đã chọn vào TCD");
+                clickTCD();
+            }
+        });
+
+        ivLCN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Đã chọn và LCN");
+                clickLCN();
+            }
+        });
+
+        ivDKT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Đã chọn vào DKT");
+                clickDKT();
+            }
+        });
+
+        ivCTH.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Đã chọn vào CTH");
+                clickCTH();
+            }
+        });
+
+        ivTTSV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Đã chọn vào TTSV");
+                clickTTSV();
+            }
+        });
+
+    }
+
+    private void clickTCD() {
+        System.out.println("Click True");
+    }
+
+    private void clickLCN() {
+        System.out.println("Click True");
+    }
+
+    private void clickDKT() {
+        System.out.println("Click True");
+    }
+
+    private void clickCTH() {
+        System.out.println("Click True");
+    }
+
+    private void clickTTSV() {
+        System.out.println("Click True");
+
     }
 }
