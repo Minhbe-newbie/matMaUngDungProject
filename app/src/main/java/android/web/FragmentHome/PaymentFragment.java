24ad7b14effe67ae9b885d3d2596635778990c74
WhyLazyDoggo <cv301101@gmail.com>
2022-12-26 16:08:42 +0700

    Pay-Fra.java: Thêm Bắt sự kiện vào Button Thanh toán frag_payment.xml: Sửa lại giao diện 1 chút. Đặt ra câu hỏi : PHẠM ĐỖ ANH MINH: GRP BOX cho "Chọn ngân hàng đâu rồi ?". Ý là điền tên ngân hàng vào là xong à?

--- app/src/main/java/android/web/FragmentHome/PaymentFragment.java
+++ app/src/main/java/android/web/FragmentHome/PaymentFragment.java
@@ -8,8 +8,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.web.R;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.sql.Connection;
+
+
 
 public class PaymentFragment extends Fragment {
+
+    private View view;
+
+    private TextView edtHaveToPay,edtHavePay,edtRemainAmountNeedToPay;
+    private Button btnTransfer;
+
+    Connection connect;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,7 +35,32 @@ public class PaymentFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_payment, container, false);
+        view = inflater.inflate(R.layout.fragment_payment, container, false);
+
+        initUi();
+
         return view;
+    }
+
+    private void initUi() {
+        edtHaveToPay = view.findViewById(R.id.edtHaveToPay);
+        edtHavePay = view.findViewById(R.id.edtHavePay);
+        edtRemainAmountNeedToPay = view.findViewById(R.id.edtRemainAmountNeedToPay);
+
+        btnTransfer = view.findViewById(R.id.btnTransfer);
+        btnTransfer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Đã bấm nút này");
+                setMakeEverythingComeTrue();
+            }
+        });
+    }
+
+    private void setMakeEverythingComeTrue() {
+        edtHavePay.setText("Thêm thông tin zô đây");
+        edtHaveToPay.setText("Thêm thông tin zô đây");
+        edtRemainAmountNeedToPay.setText("Thông tin mô");
+
     }
 }
