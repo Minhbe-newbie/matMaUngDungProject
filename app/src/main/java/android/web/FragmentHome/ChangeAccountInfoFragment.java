6d14216eb997bd8bdde5ba4fd72fb2cdaed05845
WhyLazyDoggo <cv301101@gmail.com>
2022-12-27 23:28:55 +0700

    ChangeAccountInfoFragment.java: Đã thêm sự kiện khi bấm vào nút Submit. ChangeAccountInfoFragment: Bỏ nút edit đi vì đang trong tình trạng edit rồi mà?

--- app/src/main/java/android/web/FragmentHome/ChangeAccountInfoFragment.java
+++ app/src/main/java/android/web/FragmentHome/ChangeAccountInfoFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.web.R;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,12 +16,39 @@ import android.web.R;
  */
 public class ChangeAccountInfoFragment extends Fragment {
 
+    private View view;
+    private Button btnVerificationCodeSubmit;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_change_account_info, container, false);
+        view = inflater.inflate(R.layout.fragment_change_account_info, container, false);
+
+        initUI();
+
+        return view;
+    }
+
+    private void initUI() {
+    btnVerificationCodeSubmit = view.findViewById(R.id.btnVerificationCodeSubmit);
+    btnVerificationCodeSubmit.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            System.out.println("Đã bấm nút xác nhận");
+            clickSubmit();
+        }
+    });
+
+    }
+
+    private void clickSubmit() {
+        System.out.println("Đã bấm vào nút này");
     }
 }
