8946ee732f4bb5f8ae9b985ed4de7fa60d482346
WhyLazyDoggo <cv301101@gmail.com>
2023-01-03 18:40:27 +0700

    Lịch version 1
    Tài khoản xong.
    
    Thiếu: Giao diện đổi mật khẩu - Làm soon

--- app/src/main/java/FragmentStudent/CalenderActivity.java
+++ app/src/main/java/FragmentStudent/CalenderActivity.java
@@ -1,24 +1,30 @@
 package FragmentStudent;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.web.R;
+import android.widget.CalendarView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CalenderActivity extends AppCompatActivity {
 
     ImageView backRed;
-
+    private TextView text_info;
+    private CalendarView calendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
-
         initViews();
         addEvent();
     }
+
+
 
     private void addEvent() {
         backRed.setOnClickListener(new View.OnClickListener() {
@@ -28,9 +34,28 @@ public class CalenderActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+
+                String sql= "SELECT * FROM heroku_f330b4f895ee47c.tin_chi2";
+
+
+                String date = dayOfMonth + "/" + (month+1) + "/" + year;
+                text_info.setText(date);
+
+            }
+        });
+
     }
+
+
+
 
     private void initViews() {
         backRed = findViewById(R.id.backRed);
+        calendarView = findViewById(R.id.calendarView);
+        text_info = findViewById(R.id.text_info);
     }
 }
