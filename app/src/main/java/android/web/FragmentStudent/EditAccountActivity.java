8bc1e3bbacd299cd463df3d07d6a1b25bffb3dac
BacteriaQH <hoangphuc2k1vp@gmail.com>
2023-01-03 22:45:24 +0700

    update logic forTTSVActivity

---
+++ app/src/main/java/android/web/FragmentStudent/EditAccountActivity.java
@@ -0,0 +1,14 @@
+package android.web.FragmentStudent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_account2);
+    }
+}
