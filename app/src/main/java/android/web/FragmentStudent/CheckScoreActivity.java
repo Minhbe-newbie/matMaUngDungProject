0789df938f4432bde8d675cfeccd3c50822f7618
BacteriaQH <hoangphuc2k1vp@gmail.com>
2023-01-03 23:16:23 +0700

    update layout for ScoreActivity

--- app/src/main/java/android/web/FragmentStudent/CheckScoreActivity.java
+++ app/src/main/java/android/web/FragmentStudent/CheckScoreActivity.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 import android.widget.AdapterView.OnItemSelectedListener;
 
-public class CheckScoreActivity extends AppCompatActivity implements OnItemSelectedListener{
+public class CheckScoreActivity extends AppCompatActivity {
 
     ImageView backRed;
 
@@ -31,53 +31,9 @@ public class CheckScoreActivity extends AppCompatActivity implements OnItemSelec
         addEvent();
 
 
-        Spinner spinnerNH  = (Spinner) findViewById(R.id.spinnerNH);
-        Spinner spinnerHK = (Spinner) findViewById(R.id.spinnerHK);
-        Spinner spinnerDG = (Spinner) findViewById(R.id.spinnerDG);
 
-
-
-        // Spinner click listener
-        spinnerNH.setOnItemSelectedListener(this);
-        spinnerHK.setOnItemSelectedListener(this);
-        spinnerDG.setOnItemSelectedListener(this);
-
-
-
-        // Spinner Drop down elements
-        List<String> categories = new ArrayList<String>();
-        categories.add("Automobile");
-        categories.add("Business Services");
-        categories.add("Computers");
-        categories.add("Education");
-        categories.add("Personal");
-        categories.add("Travel");
-
-        // Creating adapter for spinner
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
-
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // attaching data adapter to spinner
-        spinnerNH.setAdapter(dataAdapter);
-        spinnerHK.setAdapter(dataAdapter);
-        spinnerDG.setAdapter(dataAdapter);
 
     }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        // On selecting a spinner item
-        String item = parent.getItemAtPosition(position).toString();
-
-        // Showing selected spinner item
-        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
-    }
-    public void onNothingSelected(AdapterView<?> arg0) {
-        // TODO Auto-generated method stub
-    }
-
     private void addEvent() {
         backRed.setOnClickListener(new View.OnClickListener() {
             @Override
