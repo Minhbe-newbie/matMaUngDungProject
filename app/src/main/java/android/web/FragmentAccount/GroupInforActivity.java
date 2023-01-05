954db5ae28209ed68617a7a3186e82fabc43a9f9
BacteriaQH <hoangphuc2k1vp@gmail.com>
2023-01-06 01:34:05 +0700

    add logic to FragmentAccount and its component

---
+++ app/src/main/java/android/web/FragmentAccount/GroupInforActivity.java
@@ -0,0 +1,14 @@
+package android.web.FragmentAccount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroupInforActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_infor);
+    }
+}
