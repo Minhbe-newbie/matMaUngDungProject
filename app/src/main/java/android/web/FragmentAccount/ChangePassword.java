92b6b2a3c6e181f79fc2caa9067159657a228a3c
BacteriaQH <hoangphuc2k1vp@gmail.com>
2023-01-06 01:36:31 +0700

    add logic to FragmentAccount and its component

--- app/src/main/java/android/web/FragmentAccount/ChangePassword.java
+++ app/src/main/java/android/web/FragmentAccount/ChangePassword.java
@@ -1,23 +1,42 @@
-package android.web;
+package android.web.FragmentAccount;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.web.FragmentHome.AccountFragment;
+import android.web.Login.LoginActivity;
+import android.web.Login.ResetPasswordActivity;
+import android.web.MainHomeActivity;
+import android.web.R;
+import android.web.database.DatabaseClass;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 public class ChangePassword extends AppCompatActivity {
 
     ImageView backRed;
     private EditText edtCurPass, edtNewPass, edtNewPassAgain;
     private Button btnChangePass;
-
+    Gson gson = new Gson();
+    String MSV, pass;
+    String curPass,newPass ,newPassAgain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+        getInfor();
         initViews();
         addEvent();
     }
@@ -36,7 +55,36 @@ public class ChangePassword extends AppCompatActivity {
         btnChangePass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("Ừ bấm vào đổi mật khẩu rồi hurrayyyyy");
+                System.out.println("Click on ChangPassword");
+                curPass = edtCurPass.getText().toString();
+                newPass = edtNewPass.getText().toString();
+                newPassAgain = edtNewPassAgain.getText().toString();
+
+
+                if(curPass.isEmpty() || newPass.isEmpty() || newPassAgain.isEmpty()){
+                    Toast.makeText(ChangePassword.this, "Please fill all field", Toast.LENGTH_SHORT).show();
+                }else if(!newPass.equals(newPassAgain)){
+                    Toast.makeText(ChangePassword.this, "New password and New password again doesn't match", Toast.LENGTH_SHORT).show();
+                }else{
+                    BCrypt.Result res = BCrypt.verifyer().verify(curPass.toCharArray(), pass);
+                    System.out.println(pass);
+                    if(res.verified){
+                        String hashPassword = BCrypt.withDefaults().hashToString(12, newPass.toCharArray());
+                        System.out.println(hashPassword);
+                        int rs = DatabaseClass.updatePassword(hashPassword, MSV );
+                        System.out.println(rs);
+                        if(rs == 1){
+                            Intent i = new Intent(ChangePassword.this, MainHomeActivity.class);
+
+                            startActivity(i);
+                        }else{
+                            System.out.println("Err when update");
+                            Toast.makeText(ChangePassword.this, "Error when update password", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(ChangePassword.this, "Password is incorrect", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
 
@@ -48,6 +96,21 @@ public class ChangePassword extends AppCompatActivity {
             }
         });
 
+    }
+    private void getInfor(){
+        SharedPreferences prefs = getSharedPreferences("preference_user", MODE_PRIVATE);
+        String json = prefs.getString("user", "");
+        JSONObject userObj = gson.fromJson(json, JSONObject.class);
+        try {
+            if (userObj == null){
+                return;
+            }
+            MSV = (String) userObj.get("MaSinhVien");
+            pass = (String) userObj.get("Pass");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
 
--- app/src/main/java/android/web/FragmentAccount/GroupInforActivity.java
+++ app/src/main/java/android/web/FragmentAccount/GroupInforActivity.java
@@ -2,13 +2,29 @@ package android.web.FragmentAccount;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.web.R;
+import android.widget.ImageView;
 
 public class GroupInforActivity extends AppCompatActivity {
-
+    ImageView backRed;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_infor);
+
+
+
+        backRed= findViewById(R.id.back_red);
+        backRed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+                finish();
+            }
+        });
     }
 }
